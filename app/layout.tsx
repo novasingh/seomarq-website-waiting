95123bb2c7ff83f5fb031c0f8fbd528f8355d0d5
novaingh <novasingh24@gmail.com>
2025-11-07 23:00:28 +0530

    Add : analytics and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/next";
 import './globals.css';
 
 const inter = Inter({ 
@@ -144,6 +145,7 @@ export default function RootLayout({
       </head>
       <body className={inter.className} suppressHydrationWarning>
         {children}
+        <Analytics />
       </body>
     </html>
   );
